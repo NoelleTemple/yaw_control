1f62381d42c2aa03c480b3a969ed653bd41d3239
Noelle Lewandowski <tug48873@temple.edu>
2019-09-30 02:11:44 +0100

    yawcntl file updated needs exceptions

--- yawcntl.py
+++ yawcntl.py
@@ -27,16 +27,54 @@ print('Reading BNO055 Heading.  Press Cntl-C to quit...')
 p=GPIO.PWM(su.boardpin, su.frequency)
 p.start(0.5)
 
-des_heading = 255
+#original heading
+og_heading, roll, pitch = bno.read_euler()
+#where do you want to go?
+degrees = 60
+#clockwise if true, counter clockwise if false
+cw = true
 
 while True:
 	GPIO.setwarnings(False)
 	heading, roll, pitch = bno.read_euler()
 	sys, gyro, accel, mag = bno.get_calibration_status()
 	print('Heading = {0:0.2F}'.format(heading))
-	if 
-		p.ChangeDutyCycle(dc)
+	if cw == true:
+		#set servo all the way to the left
+		dc = 2.5
+		new_heading = og_heading + degrees
+		if new_heading > 360:
+			des_heading = new_heading-360
+		else:
+			des_heading = new_heading
+		x = True
+		while x == True:
+			p.ChangeDutyCycle(dc)
+			time.sleep(0.25)
+			heading, roll, pitch = bno.read_euler()
+			if heading < des_heading+5 && heading > des_heading-5:
+				x=False
+			else:
+				dc=dc+1
+			#need an exception if it doesn't catch it (when does it just pass,
+			#could change step size and go back instead of looking for certain point
+			#what happens if it just never reads where it's in those bounds?
 		print('duty cycle: {}'.format(dc))
-	time.sleep(1)
-
+	else:
+		dc = 12.5
+		new_heading = og_heading-degress
+		if new_heading < 0:
+			des_heading = new_heading + 360
+		else:
+			des_heading = new_heading
+		x = True
+		while x == True:
+			p.ChangeDutyCycle(dc)
+			time.sleep(0.25)
+			heading, roll, pitch = bno.read_euler()
+			if heading < des_heading+5 && heading > des_heading-5:
+				x = False
+			else:
+				dc = dc-1
+			#need that exception from earlier here.
 GPIO.cleanup()
